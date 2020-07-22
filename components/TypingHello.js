227c566f5a88e4f0a13885656a26f00943ed9215
MikeN64 <nguyen.michael.van@gmail.com>
2020-07-22 00:54:03 -0400

    Completed TypingHello animation

--- components/TypingHello.js
+++ components/TypingHello.js
@@ -2,7 +2,10 @@ import styles from './typing_hello.module.css'
 
 const TypingHello = () => {
 	return(
-		<div>
+		<div className={styles.typing}>
+			<h1>Hello,</h1>
+			<h1>I'm Michael.</h1>
+			<h2>Software Engineer | Full-Stack Developer</h2>
 		</div>
 	)
 }
