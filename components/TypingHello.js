4fc4eb7d9571d5b0d945622ee8caabff40e1be76
MikeN64 <nguyen.michael.van@gmail.com>
2020-07-22 11:27:18 -0400

    Minimized text with Software Engineer to be responsive to mobile

--- components/TypingHello.js
+++ components/TypingHello.js
@@ -5,7 +5,7 @@ const TypingHello = () => {
 		<div className={styles.typing}>
 			<h1>Hello,</h1>
 			<h1>I'm Michael.</h1>
-			<h2>Software Engineer | Full-Stack Developer</h2>
+			<h2>Full Stack Engineer</h2>
 		</div>
 	)
 }
